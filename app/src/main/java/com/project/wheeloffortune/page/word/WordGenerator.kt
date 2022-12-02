68cd5f9c4f35b04ffd738237c4663eef4758b8bf
s185736 <65380030+s185736@users.noreply.github.com>
2022-12-02 14:05:49 +0100

    A Word Generator has been added.

---
+++ app/src/main/java/com/project/wheeloffortune/page/word/WordGenerator.kt
@@ -0,0 +1,34 @@
+//source: https://developer.android.com/codelabs/basic-android-kotlin-training-lists#0
+package com.project.wheeloffortune.page.word
+
+import kotlin.random.Random
+
+class WordGenerator() {
+
+    private val availableWords: List<String> = listOf("Android Studio", "IntelliJ Ultimate", "Machine Code")
+
+    val randWordGenrating: String
+        get() {
+            val selectedWord: List<String> = availableWords
+            println("Word is generated.")
+            val randIn: Int = Random.nextInt(selectedWord.size)
+            return selectedWord[randIn]
+        }
+
+    fun hiddenLetters(wordSentence: String): String {
+        var letters = buildString { }
+        wordSentence.run {
+            forEach(
+                action = fun(char: Char) {
+                    letters += when (char) {
+                        ' ' -> " "
+                        else -> {
+                            "?"
+                        }
+                    }
+                },
+            )
+        }
+        return letters
+    }
+}
