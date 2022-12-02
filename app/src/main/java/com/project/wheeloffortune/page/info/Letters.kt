df85900a4c4f660d94da498e6fcf0991a7b6f81f
s185736 <65380030+s185736@users.noreply.github.com>
2022-12-02 13:25:37 +0100

    Informations of Game.

---
+++ app/src/main/java/com/project/wheeloffortune/page/info/Letters.kt
@@ -0,0 +1,47 @@
+package com.project.wheeloffortune.page.info
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.project.wheeloffortune.handler.GameHandler
+import com.project.wheeloffortune.page.PickLetter
+
+class Letters {
+    @Composable
+    fun LetterCons(handler: GameHandler){
+        Spacer(modifier = Modifier.size(110.dp))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 30.dp)){
+            PickLetter("B", handler)
+            PickLetter("C", handler)
+            PickLetter("D", handler)
+            PickLetter("F", handler)
+            PickLetter("G", handler)
+            PickLetter("H", handler)
+            PickLetter("J", handler)
+        }
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 30.dp)){
+            PickLetter("K", handler)
+            PickLetter("L", handler)
+            PickLetter("M", handler)
+            PickLetter("N", handler)
+            PickLetter("P", handler)
+            PickLetter("Q", handler)
+            PickLetter("R", handler)
+        }
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 30.dp)) {
+            PickLetter("S", handler)
+            PickLetter("T", handler)
+            PickLetter("V", handler)
+            PickLetter("W", handler)
+            PickLetter("X", handler)
+            PickLetter("Z", handler)
+        }
+    }
+}
